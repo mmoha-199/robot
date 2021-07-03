681a097767418db555115dafd993060d51592733
mohamed <76411950+mmoha-199@users.noreply.github.com>
2021-07-03 15:41:17 +0100

    nav fix done

--- js/app.js
+++ js/app.js
@@ -1,7 +1,14 @@
 const NAVBUTON = document.getElementById('show-nav');
 const navLIst = document.getElementById('show-ln');
+const DisCInn = document.getElementById('discover'); 
 
-const tooGleNav =()=>{navLIst.classList.toggle('visible')};
+
+
+const tooGleNav =()=>{
+    navLIst.classList.toggle('visible');
+    DisCInn.classList.toggle('down');
+};
+
 
 NAVBUTON.addEventListener('click',tooGleNav);
 
