c3b8a9480194f47123e35410eb7eaa8b81a392ea
mohamed <76411950+mmoha-199@users.noreply.github.com>
2021-07-09 23:50:09 +0100

    sort prob

--- js/app.js
+++ js/app.js
@@ -5,55 +5,44 @@ const CatGInn = document.getElementById('categories');
 const ShopInn = document.getElementById('shop');
 const AdrsInn = document.getElementById('contact');
 const myNav = document.querySelector('nav');
-
-
-
-
-
-
-
-const sctin = document.getElementsByClassName('section');
 const navMenu = document.createElement("ul");
+
 navMenu.setAttribute('class' ,'nav-list');
-const MYNAV = document.querySelector('nav'); 
 
-document.body.onload = addNavEl;
+let n ='nav';
+const sctin = document.getElementsByClassName('section'); 
+document.body.onload = addNavEl();
 
-function addNavEl () {
-  for( y = 0 ; y < sctin.length ;y++){
-    const linkName = sctin[y].id;
+function addNavEl(){
+  for( const baseSect of sctin){
+    const linkName = baseSect.id;
     const linkNCapt = linkName.toUpperCase();
-    navMenu.innerHTML += `<li><a href='#${linkName}'>${linkNCapt}</a></li>`;
-  }
-  MYNAV.appendChild(navMenu); 
-}
-
-const aelements = navMenu.getElementsByTagName('a');
-
-
-function sectInView(){
-  const getscaugth=(el)=>{
+    const linkCl = 'links';
     
-      rect = el.getBoundingClientRect() ;
-      return rect.top <= 100 && rect.bottom >= 50
+    
+    navMenu.innerHTML +=`<li><a href=#${linkName} id=${linkName}${n} class=${linkCl}>${linkNCapt}</a></li>`;
   };
-   
-  for(const link of aelements){
+  myNav.appendChild(navMenu);
+};
 
-    window.addEventListener(
-      'scroll',
-      function(){
-        if(getscaugth(sect)){
-          link.classList.add('newColor');
-        }else{
-          link.classList.remove('newColor');
-        }
-      }
-    );
+function highLlinks(){
+  const getscaugth=(el)=>{
+      const rect = el.getBoundingClientRect();
+       return (rect.top <= 100 && rect.bottom >= 50);
+  };
+  for(const sect of sctin){
+    const sectInScrn = document.getElementById(sect.id);
+    const readlink = document.getElementById(sect.id + n);
+    window.addEventListener('scroll',function(){
+      if(getscaugth(sectInScrn)){
+        readlink.classList.add('newColor');
+      }else{      
+        readlink.classList.remove('newColor');
+      };
+    });  
   }
 };
-sectInView();
-
+highLlinks();
 
 //toogle the nav menu and the first main section
 const tooGleNav =()=>{
@@ -68,7 +57,7 @@ function tooGlSections(a){
   const b = a.firstElementChild.nextElementSibling;
   const ttgg=()=>{b.classList.toggle('visible')};
   x.addEventListener('click',ttgg);
-}
+};
 
 tooGlSections(DisCInn);
 tooGlSections(LatEInn);
@@ -78,27 +67,33 @@ tooGlSections(ShopInn);
 
 function scroOllto(sect){
   sect.scrollIntoView();
-}
+};
+
 scroOllto(DisCInn);
 scroOllto(LatEInn);
 scroOllto(CatGInn);
 scroOllto(ShopInn);
 scroOllto(AdrsInn);
+
 let prevScr = window.pageYOffset;
+function navControl(){
+  const naVDowns = function(){
+    let currScr = window.pageYOffset;
+    if(prevScr > currScr){
+      myNav.style.top='0';
+    }else{
+     myNav.style.top ='-70px';
+    };
+    prevScr = currScr;
+  };
+  
+  function mmyConstw(){
+    window.addEventListener('scroll',naVDowns);
+  };
+  
+  
+  window.setTimeout(mmyConstw(),3000);
+}
 
-const naVDowns = function(){
-  let currScr = window.pageYOffset;
- if(prevScr > currScr){
-   myNav.style.top='0';
- }else{
-   myNav.style.top ='-70px';
- }
- prevScr = currScr;
-};
-function mmyConstw(){
-  window.addEventListener('scroll',naVDowns);
-};
 
-
-window.setTimeout(mmyConstw(),3000);
 
