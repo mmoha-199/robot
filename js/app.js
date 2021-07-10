0668e749641ef902b094cf6b3c60e2fa2df8a318
mohamed <76411950+mmoha-199@users.noreply.github.com>
2021-07-10 11:46:05 +0100

    adjust disc margin

--- js/app.js
+++ js/app.js
@@ -12,27 +12,28 @@ navMenu.setAttribute('class' ,'nav-list');
 let n ='nav';
 const sctin = document.getElementsByClassName('section'); 
 document.body.onload = addNavEl();
-
+//create a dynamic navigation list with 'li' s items
 function addNavEl(){
   for( const baseSect of sctin){
     const linkName = baseSect.id;
     const linkNCapt = linkName.toUpperCase();
     const linkCl = 'links';
-    
-    
     navMenu.innerHTML +=`<li><a href=#${linkName} id=${linkName}${n} class=${linkCl}>${linkNCapt}</a></li>`;
   };
   myNav.appendChild(navMenu);
 };
 
 function highLlinks(){
+  //create a function to get the width and hight of an element in the view
   const getscaugth=(el)=>{
       const rect = el.getBoundingClientRect();
-       return (rect.top <= 100 && rect.bottom >= 50);
+       return (rect.top <= 120 && rect.bottom >= 50);
   };
+  //loop in the sections and the nav list
   for(const sect of sctin){
     const sectInScrn = document.getElementById(sect.id);
     const readlink = document.getElementById(sect.id + n);
+    //check if true then add the new class to the links
     window.addEventListener('scroll',function(){
       if(getscaugth(sectInScrn)){
         readlink.classList.add('newColor');
@@ -64,7 +65,7 @@ tooGlSections(LatEInn);
 tooGlSections(CatGInn);
 tooGlSections(ShopInn);
 
-
+//when click a link in the nav list scroll to section using the reference
 function scroOllto(sect){
   sect.scrollIntoView();
 };
@@ -75,25 +76,23 @@ scroOllto(CatGInn);
 scroOllto(ShopInn);
 scroOllto(AdrsInn);
 
+//controle the nav height and appearence when scrolling the page
 let prevScr = window.pageYOffset;
-function navControl(){
-  const naVDowns = function(){
-    let currScr = window.pageYOffset;
-    if(prevScr > currScr){
-      myNav.style.top='0';
-    }else{
-     myNav.style.top ='-70px';
-    };
-    prevScr = currScr;
-  };
-  
-  function mmyConstw(){
-    window.addEventListener('scroll',naVDowns);
-  };
-  
-  
-  window.setTimeout(mmyConstw(),3000);
-}
+
+window.setTimeout(function(){
+  window.addEventListener(
+    'scroll',function(){
+      let currScr = window.pageYOffset;
+      if(prevScr > currScr){
+        myNav.style.top='0px';
+      }else{
+        myNav.style.top ='-223px';
+      };
+      prevScr = currScr;
+    });
+},3000);
+
+
 
 
 
